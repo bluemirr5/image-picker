5f3fdf651c782e61d10f57fd9823253fab63f0bb
GS.Won <bluemirr5@gmail.com>
2014-07-17 10:47:58 +0900

    refactoring source

--- background.js
+++ background.js
@@ -7,6 +7,6 @@
 // 		console.log(param);
 // 	});
 // })
-chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab){
-	chrome.tabs.update(tabId, {url:"javascript:function naver(q){void(z=q.body.appendChild(q.createElement('script'))); void(z.language='javascript');void(z.type='text/javascript');void(z.src='http://userscripts.org/scripts/source/61326.user.js');}function selfw(w){try{naver(w.document);}catch(e){}for(var i =0;i<w.frames.length;i++){try{selfw(w.frames[i]);}catch(e){}}}selfw(self);(function(){var e, i, all;document.onselectstart=null;document.oncontextmenu=null;all=document.getElementsByTagName('*');for(i=0;i<all.length;i+=1){e=all[i];e.onselectstart=null;e.oncontextmenu=null;}})();"}, function(tab){});
-})
+//chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab){
+//	chrome.tabs.update(tabId, {url:"javascript:function naver(q){void(z=q.body.appendChild(q.createElement('script'))); void(z.language='javascript');void(z.type='text/javascript');void(z.src='http://userscripts.org/scripts/source/61326.user.js');}function selfw(w){try{naver(w.document);}catch(e){}for(var i =0;i<w.frames.length;i++){try{selfw(w.frames[i]);}catch(e){}}}selfw(self);(function(){var e, i, all;document.onselectstart=null;document.oncontextmenu=null;all=document.getElementsByTagName('*');for(i=0;i<all.length;i+=1){e=all[i];e.onselectstart=null;e.oncontextmenu=null;}})();"}, function(tab){});
+//})
--- popup.js
+++ popup.js
@@ -1,8 +1,4 @@
 function ImageFinderCTRL($scope) {
-	// $('#container').masonry({
-	// 	columnWidth: 200,
-	// 	itemSelector: '.item'
-	// });
 	$(function(){
   	$('#container').masonry({
 	    // options
@@ -10,80 +6,139 @@ function ImageFinderCTRL($scope) {
 	  });
 	});
 
-	var dup = {};
-	$scope.imageArray = [];
+
 	chrome.windows.getCurrent(function(w){ // Select Current Window
-		chrome.tabs.query({windowId:w.id, active:true}, function(tabs){ // Select Current Tab
-			var tab_Id = -1;
-			if(tabs.length > 0) {
-				tab_Id = tabs[0].id;
-				chrome.pageCapture.saveAsMHTML({tabId:tab_Id}, function(htmlData){
-					var reader = new FileReader();
-					reader.addEventListener("loadend", function() {
-					   // reader.result contains the contents of blob as a typed array
-						var boundary = getBoundaryFromBlobStr(reader.result);
-						var contents = reader.result.split(boundary);
-						for(var i = 0; contents != null && i < contents.length; i++) {
-							var content = contents[i];
-							var fixs = content.split("\n", 4);
-							if(fixs != null && fixs.length > 2) {
-								if(fixs[1].indexOf("Content-Type: image") != -1) { // Image Tag
-									var obj = {};
-									var contentsTypeArray = fixs[1].split("Content-Type: ");
-									var contentsLocationArray = fixs[3].split("Content-Location: ");
-									obj.contentType = contentsTypeArray[1];
-									obj.location = "";
-									obj.location = contentsLocationArray[1];
-									obj.location = obj.location.replace("=", "");
-									obj.location = obj.location.replace("\n", "");
-									obj.location = obj.location.replace(/[\n\r]/g, '');
-									obj.location = obj.location.replace('"', '')
-									obj.location = obj.location.replace(/"/g, '')
-									if(obj.location.indexOf("http") != -1 && dup[obj.location] == null) {
-										dup[""+obj.location] = "img";
-										var nameTemps = obj.location.split("/");
-										obj.fileName = nameTemps[nameTemps.length-1]
-										$scope.imageArray.push(obj);
-									}
-								} else if(fixs[1].indexOf("Content-Type: text/css") != -1) { // CSS Background
-									var temp = content.split("background-image")
-									var cssTempArray = temp.slice(1, temp.length);
-									for(var j = 0; j < cssTempArray.length; j++) {
-										var urlTempArray = cssTempArray[j].split(";");
-										if(urlTempArray != null && urlTempArray.length > 0) {
-											var urlRealTempArray = urlTempArray[0].split(")");
-											if(urlRealTempArray != null && urlRealTempArray.length > 0) {
-												var finala = urlRealTempArray[0].split("(");
-												if(finala != null && finala.length > 1) {
-													var obj = {};
-													obj.location = "";
-													obj.location = finala[1];
-													obj.location = obj.location.replace("=", "");
-													obj.location = obj.location.replace("\n", "");
-													obj.location = obj.location.replace(/[\n\r]/g, '');
-													obj.location = obj.location.replace('"', '')
-													obj.location = obj.location.replace(/"/g, '')
-													var nameTemps = obj.location.split("/");
-													obj.fileName = nameTemps[nameTemps.length-1]
-													if(obj.location.indexOf("http") != -1 && dup[obj.location] == null) {
-														dup[""+obj.location] = "css";
-														$scope.imageArray.push(obj);
-													}
-												}
-											}
-										}
-									}
-								}
-							}
-						}
-						$scope.$apply();
-					});
-					reader.readAsText(htmlData);
-				});
-			}
-		});
+	    var selectTabParam = {};
+	    selectTabParam.windowId = w.id;
+	    selectTabParam.active = true;
+	    var selectTabCallBack = function(tabs) {
+	        if(tabs.length <= 0) {
+	            return;
+	        }
+
+	        chrome.pageCapture.saveAsMHTML({tabId:tabs[0].id}, function(htmlData){
+	            var reader = new FileReader();
+	            reader.addEventListener("loadend", function() {
+                    var boundary = getBoundaryFromBlobStr(reader.result);
+                    var contents = reader.result.split(boundary);
+                    for(var i = 0; contents != null && i < contents.length; i++) {
+                        getImageFromContents(contents[i])
+                    }
+	            });
+	            reader.readAsText(htmlData);
+	        });
+	    };
+	    chrome.tabs.query(selectTabParam, selectTabCallBack);
 	});
-	
+
+    function getImageFromContents(contentsString) {
+        console.log(contentsString);
+        if(isImageContentsType(contentsString)) {
+            var contentsUrl = getContentsLocation(contentsString);
+            console.log(contentsUrl);
+        }
+    }
+
+    function isImageContentsType(contentsString) {
+        var retValue = false;
+        var contentsStringSplited = contentsString.split("Content-Type: ", 2)
+        if(contentsStringSplited.length > 2) {
+            var newLineSplited = contentsStringSplited[1].split("\n", 2);
+            if(newLineSplited.length > 2) {
+                var contentsType = newLineSplited[0]
+                console.log(contentsType);
+                retValue = contentsType.contains("image")
+            }
+        }
+        return retValue;
+    }
+
+    function getContentsLocation(contentsString) {
+        var contentsLocation = "";
+        var contentsStringSplited = contentsString.split("Content-Location: ", 2)
+        if(contentsStringSplited.length > 2) {
+            var newLineSplited = contentsStringSplited[1].split("\n", 2);
+            if(newLineSplited.length > 2) {
+                contentsLocation = newLineSplited[0]
+            }
+        }
+        return contentsLocation;
+    }
+
+
+//	var dup = {};
+//	$scope.imageArray = [];
+//	chrome.windows.getCurrent(function(w){ // Select Current Window
+//		chrome.tabs.query({windowId:w.id, active:true}, function(tabs){ // Select Current Tab
+//			var tab_Id = -1;
+//			if(tabs.length > 0) {
+//				tab_Id = tabs[0].id;
+//				chrome.pageCapture.saveAsMHTML({tabId:tab_Id}, function(htmlData){
+//					var reader = new FileReader();
+//					reader.addEventListener("loadend", function() {
+//					   // reader.result contains the contents of blob as a typed array
+//						var boundary = getBoundaryFromBlobStr(reader.result);
+//						var contents = reader.result.split(boundary);
+//						for(var i = 0; contents != null && i < contents.length; i++) {
+//							var content = contents[i];
+//							var fixs = content.split("\n", 4);
+//							if(fixs != null && fixs.length > 2) {
+//								if(fixs[1].indexOf("Content-Type: image") != -1) { // Image Tag
+//									var obj = {};
+//									var contentsTypeArray = fixs[1].split("Content-Type: ");
+//									var contentsLocationArray = fixs[3].split("Content-Location: ");
+//									obj.contentType = contentsTypeArray[1];
+//									obj.location = "";
+//									obj.location = contentsLocationArray[1];
+//									obj.location = obj.location.replace("=", "");
+//									obj.location = obj.location.replace("\n", "");
+//									obj.location = obj.location.replace(/[\n\r]/g, '');
+//									obj.location = obj.location.replace('"', '')
+//									obj.location = obj.location.replace(/"/g, '')
+//									if(obj.location.indexOf("http") != -1 && dup[obj.location] == null) {
+//										dup[""+obj.location] = "img";
+//										var nameTemps = obj.location.split("/");
+//										obj.fileName = nameTemps[nameTemps.length-1]
+//										$scope.imageArray.push(obj);
+//									}
+//								} else if(fixs[1].indexOf("Content-Type: text/css") != -1) { // CSS Background
+//									var temp = content.split("background-image")
+//									var cssTempArray = temp.slice(1, temp.length);
+//									for(var j = 0; j < cssTempArray.length; j++) {
+//										var urlTempArray = cssTempArray[j].split(";");
+//										if(urlTempArray != null && urlTempArray.length > 0) {
+//											var urlRealTempArray = urlTempArray[0].split(")");
+//											if(urlRealTempArray != null && urlRealTempArray.length > 0) {
+//												var finala = urlRealTempArray[0].split("(");
+//												if(finala != null && finala.length > 1) {
+//													var obj = {};
+//													obj.location = "";
+//													obj.location = finala[1];
+//													obj.location = obj.location.replace("=", "");
+//													obj.location = obj.location.replace("\n", "");
+//													obj.location = obj.location.replace(/[\n\r]/g, '');
+//													obj.location = obj.location.replace('"', '')
+//													obj.location = obj.location.replace(/"/g, '')
+//													var nameTemps = obj.location.split("/");
+//													obj.fileName = nameTemps[nameTemps.length-1]
+//													if(obj.location.indexOf("http") != -1 && dup[obj.location] == null) {
+//														dup[""+obj.location] = "css";
+//														$scope.imageArray.push(obj);
+//													}
+//												}
+//											}
+//										}
+//									}
+//								}
+//							}
+//						}
+//						$scope.$apply();
+//					});
+//					reader.readAsText(htmlData);
+//				});
+//			}
+//		});
+//	});
 
 	function getBoundaryFromBlobStr(str) {
 		var tempArray = str.split("\tboundary=\"");
@@ -102,7 +157,4 @@ function ImageFinderCTRL($scope) {
 		}
 		
 	};
-	/*
-	javascript:function r(d){d.oncontextmenu=null;d.onselectstart=null;d.ondragstart=null;d.onkeydown=null;d.onmousedown=null;  d.body.oncontextmenu=null;d.body.onselectstart=null;d.body.ondragstart=null;d.body.onkeydown=null; d.body.onmousedown=null;}var  tb=document.all.tags('BODY');if(tb.length==0) {for(var i=0;i<top.frames.length;i++){r(top.frames[i].document);}}else{r(document);}
-	*/
 }
